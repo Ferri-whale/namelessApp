a8c627c44e9031d6634c5744581f57258b3b85f9
Patrick <pye@connectedlab.com>
2015-07-05 18:11:44 -0400

    Initial Commit

---
+++ namelessApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  namelessApp
+//
+//  Created by Patrick Ye on 2015-07-05.
+//  Copyright (c) 2015 FerriWhale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
